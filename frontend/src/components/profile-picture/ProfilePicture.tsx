6bd064294746697cd239236d42c34a64f04ebf83
Andrej-Dunda <dundaandrej@seznam.cz>
2023-12-29 18:01:21 +0100

    fixed profile picture upload

--- frontend/src/components/profile-picture/ProfilePicture.tsx
+++ frontend/src/components/profile-picture/ProfilePicture.tsx
@@ -1,12 +1,24 @@
 import './ProfilePicture.scss'
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../App';
 
 const ProfilePicture = (props: any) => {
+
+  // State to manage image source URL
+  const [imgSrc, setImgSrc] = useState<string>(`http://127.0.0.1:5000/profile-picture/${props.userId}`);
+  const userInfo = useContext(UserContext)
+
+  useEffect(() => {
+    // Append a timestamp or a random query parameter to force reload the image from the server
+    setImgSrc(`http://127.0.0.1:5000/profile-picture/${props.userId}?${new Date().getTime()}`);
+  }, [props.userId, userInfo.profile_picture])
+
   return (
     <img
       className={`profile-picture ${props.className}`}
-      src={`http://127.0.0.1:5000/profile-picture/${props.userId}`}
+      src={imgSrc}
       alt=""
     />
-  )
-}
+  );
+};
 export default ProfilePicture;
