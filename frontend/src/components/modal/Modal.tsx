a446f5f674fd5ac31b450111a1a62a18cbe895c9
Andrej-Dunda <dundaandrej@seznam.cz>
2023-12-29 18:01:56 +0100

    improved modal performance

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,12 +1,63 @@
-import './Modal.scss'
+import './Modal.scss';
+import { useEffect, useRef } from 'react';
 
 const Modal = ({ isOpen, onClose, onSubmit, children, submitContent, cancelContent }: any) => {
+  const modalRef = useRef<HTMLDivElement>(null); // Refer to the modal content
+  let clickStartedInside = false; // Track where the click started
+
+  useEffect(() => {
+    const handleKeyPress = (event: any) => {
+      switch (event.key) {
+        case 'Enter':
+          onSubmit();
+          break;
+        case 'Escape':
+          onClose();
+          break;
+        default:
+          break;
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeyPress);
+    } else {
+      document.removeEventListener('keydown', handleKeyPress);
+    }
+
+    // Cleanup function to remove the event listener
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [isOpen, onClose, onSubmit]); // Depend on the isOpen state
+
+  const handleOverlayClick = (event: any) => {
+    // Close only if clicked directly on overlay and the click didn't start inside the modal
+    if (event.target.id === "overlay" && !clickStartedInside) {
+      onClose();
+    }
+    clickStartedInside = false; // Reset the flag
+  };
+
+  const handleModalMouseDown = () => {
+    // Indicate the click started inside the modal
+    clickStartedInside = true;
+  };
+  
   return (
     <>
       {
         isOpen &&
-        <div className='modal modal-overlay'>
-          <div className="modal-content">
+        <div
+          className='modal modal-overlay'
+          id='overlay'
+          onClick={handleOverlayClick}
+        >
+          <div
+            className="modal-content"
+            ref={modalRef}
+            onMouseDown={handleModalMouseDown}
+          >
             <div className="modal-body">
               {children}
             </div>
