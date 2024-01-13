3238ccbe6d997770f4d06e06935e825f8684eb42
Andrej-Dunda <dundaandrej@seznam.cz>
2024-01-13 22:50:39 +0100

    added show more paragraph component

---
+++ frontend/src/components/text-paragraph/TextParagraph.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import './TextParagraph.scss'
+
+interface iTextParagraph {
+  className?: string;
+  text: string;
+  characterLimit?: number;
+  onShowMoreToggle?: () => void;
+}
+
+const TextParagraph: React.FC<iTextParagraph> = ({ className, text, characterLimit, onShowMoreToggle }) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  useEffect(() => {
+    onShowMoreToggle && onShowMoreToggle();
+  }, [isExpanded, onShowMoreToggle])
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  }
+
+  return (
+    <p className={`text-paragraph ${className}`}>
+      {
+        characterLimit && text.length > characterLimit && !isExpanded ? (
+          <>
+            {text.substring(0, characterLimit)}
+          </>
+        ) : (
+          text
+        )
+      }
+      {
+        characterLimit && text.length > characterLimit ? (
+          <span className='show-more' onClick={toggleExpand}>
+            {isExpanded ? 'Zobrazit méně' : 'Zobrazit více'}
+          </span>
+        ) : null
+      }
+    </p>
+  )
+}
+export default TextParagraph;
